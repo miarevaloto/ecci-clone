c0d6c13a0c3d29e2b3f48350827bd4d33031b9fc
miarevaloto <juanmi.arevaloto@ecci.edu.co>
2026-05-15 10:42:41 -0500

    Update Navbar.vue

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -3,16 +3,16 @@
     <img src="/logo-ecci.png" alt="Logo ECCI" class="logo" />
     <ul>
       <li>
-        <button @click="goToUniversity('Harvard', 'https://www.harvard.edu')">Inicio</button>
+        <button @click="goToUniversity('Inicios', 'https://es.wikipedia.org/wiki/Universidad')">Inicio</button>
       </li>
       <li>
         <button @click="goToUniversity('Programas de Ingeniería', 'https://www.google.com/search?q=PROGRAMAS+DE+INGENIERIA+EN+COLOMBIA&oq=PROGRAMAS+DE+INGENIERIA+EN+COLOMBIA&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTEyNzM1ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8')">Programas</button>
       </li>
       <li>
-        <button @click="goToUniversity('MIT News', 'https://news.mit.edu')">Noticias</button>
+        <button @click="goToUniversity('Noticias Universidades Bogota', 'https://www.google.com/search?q=noticias+universidades+en+bogota+colombia&sca_esv=e1aed3f059b28a74&biw=1920&bih=953&sxsrf=ANbL-n6OAMdotlLHFgivcxGAZ_tNKymeXA%3A1778859549131&ei=HT4HatnBB7WXwbkPuLij2Qw&oq=noticias+universidades+en+bogota&gs_lp=Egxnd3Mtd2l6LXNlcnAiIG5vdGljaWFzIHVuaXZlcnNpZGFkZXMgZW4gYm9nb3RhKgIIATIFECEYoAEyBRAhGKABMgUQIRifBTIFECEYnwUyBRAhGJ8FMgUQIRifBUiMNFCtCljOJXABeAGQAQCYAZQBoAHhDaoBBDAuMTS4AQPIAQD4AQGYAg-gApoOwgIKEAAYRxjWBBiwA8ICBhAAGBYYHsICCBAAGIAEGKIEwgIFEAAY7wWYAwCIBgGQBgiSBwQxLjE0oAeRNLIHBDAuMTS4B5YOwgcGMC4xMi4zyAcigAgB&sclient=gws-wiz-serp')">Noticias</button>
       </li>
       <li>
-        <button @click="goToUniversity('UNAL Contacto', 'https://unal.edu.co/contacto')">Contacto</button>
+        <button @click="goToUniversity('Contactos', 'https://www.google.com/search?q=contactos+universidades&sca_esv=e1aed3f059b28a74&sxsrf=ANbL-n6W78zWzNwiCh7stXyQ4mX2g3kNDQ:1778859651784&udm=1&lsack=gz4Hat6yL4T8wbkP-O6N8Qs&sa=X&ved=2ahUKEwiegJnN0LuUAxUEfjABHXh3I74QjGp6BAgsEAA&biw=1920&bih=953&dpr=1')">Contacto</button>
       </li>
     </ul>
   </nav>
