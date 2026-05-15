864ab8277a3c1abe8f2a6e031821a40756d8f1ba
miarevaloto <juanmi.arevaloto@ecci.edu.co>
2026-05-15 11:00:10 -0500

    Update Agenda.vue

--- frontend/src/components/Agenda.vue
+++ frontend/src/components/Agenda.vue
@@ -22,7 +22,7 @@
     </div>
 
     <div class="center">
-      <button class="btn">Ver más eventos</button>
+      <button class="btn", @click="goToUniversity('Eventos', 'https://es.wikipedia.org/wiki/Universidad')">Ver más eventos</button>
     </div>
   </section>
 </template>
